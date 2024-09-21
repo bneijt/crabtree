66a4b8a2385973c48d8985b2d91ddc76569eb913
Bram Neijt <bram@neijt.nl>
2024-09-21 22:14:08 +0200

    Clean up a bit

--- src/render_cmd.rs
+++ src/render_cmd.rs
@@ -39,11 +39,6 @@ pub async fn render_database(database: TomlFile) -> String {
     let mut members = database.member;
     members.sort_by(|a, b| a.date_of_birth.cmp(&b.date_of_birth));
 
-    // let header = [String::from("flowchart TD")];
-    // let member_lines = members
-    //     .iter()
-    //     .map(|member| format!("{}({})", member.id.clone().unwrap(), member.display));
-
     // // For each parent combination, create a combine node: ⚤
     let joining_node_duplicates: Vec<JoiningNode> = members
         .iter()
@@ -53,19 +48,6 @@ pub async fn render_database(database: TomlFile) -> String {
                 .clone()
                 .map(|parents| {
                     let hash_result = murmur3_32(&mut Cursor::new(parents.join("|")), 0).unwrap();
-                    // let mut nds: Vec<String> = Vec::new();
-                    // nds.push(format!("{:x}(⚤)", hash_result));
-
-                    // nds.extend(
-                    //     parents
-                    //         .iter()
-                    //         .map(|parent_id| format!("{} --- {:x}", parent_id, hash_result)),
-                    // );
-                    // nds.push(format!(
-                    //     "{:x} --- {}",
-                    //     hash_result,
-                    //     member.id.clone().unwrap()
-                    // ));
 
                     vec![JoiningNode {
                         id: hash_result.to_string(),
@@ -105,43 +87,6 @@ pub async fn render_database(database: TomlFile) -> String {
             (member.id.clone().unwrap(), age as u32)
         })
         .collect();
-
-    // let mut parent_combination_nodes: Vec<String> = members
-    //     .iter()
-    //     .flat_map(|member| {
-    //         let combination_nodes: Vec<String> = member
-    //             .parents
-    //             .clone()
-    //             .map(|parents| {
-    //                 let hash_result = murmur3_32(&mut Cursor::new(parents.join("|")), 0).unwrap();
-    //                 let mut nds: Vec<String> = Vec::new();
-    //                 nds.push(format!("{:x}(⚤)", hash_result));
-    //                 nds.extend(
-    //                     parents
-    //                         .iter()
-    //                         .map(|parent_id| format!("{} --- {:x}", parent_id, hash_result)),
-    //                 );
-    //                 nds.push(format!(
-    //                     "{:x} --- {}",
-    //                     hash_result,
-    //                     member.id.clone().unwrap()
-    //                 ));
-    //                 nds
-    //             })
-    //             .unwrap_or(Vec::new());
-    //         combination_nodes
-    //     })
-    //     .collect();
-    // parent_combination_nodes.sort_unstable();
-    // parent_combination_nodes.dedup();
-
-    //Join lines into a single newline split String
-    // let content: Vec<String> = header
-    //     .into_iter()
-    //     .chain(member_lines)
-    //     .chain(parent_combination_nodes)
-    //     .collect();
-    // content.join("\n").into();
 
     let mut context = Context::new();
     context.insert("members", &members);
